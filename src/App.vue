661994d9de1a14d5647539b7edacde9524f02501
Morgaine <morgainedv@gmail.com>
2019-11-27 15:08:34 +0000

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="html">
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return {
+    country: [],
+    selectedCountry: null
+    };
+  },
+mounted(){
+  fetch('https://restcountries.eu/rest/v2/all')
+  .then(res => res.json())
+  .then(countries => this.countries = countries)
+}
+
+
+
+
+
+
+
+
+
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
