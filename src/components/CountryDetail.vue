c4302aa69bcd97c3f4e00942c437d6a27a6e2b62
Morgaine <morgainedv@gmail.com>
2019-11-27 15:17:15 +0000

    Country Detail Updated

--- src/components/CountryDetail.vue
+++ src/components/CountryDetail.vue
@@ -1,9 +1,15 @@
 <template lang="html">
-
+<div v-if='country'>
+  <h3>{{country.name}}</h3>
+  <p> Capital: {{country.capital}}</p>
+  <p> Population: {{country.population}}</p>
+</div> 
 </template>
 
 <script>
 export default {
+  name: 'country-detail',
+  props: ['country']
 }
 </script>
 
